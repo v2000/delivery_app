8e52b8a829254c96291db545a1e5316304d2834e
v2000 <vrpr2008@gmail.com>
2014-04-16 11:52:42 +0200

    add google map and controller for map

--- app/js/controllers/MapController.js
+++ app/js/controllers/MapController.js
@@ -2,13 +2,33 @@
 
 foodMeApp.controller('MapController',
     function MapController($scope, $http, UserService, $location, aService) {
-    $scope.map = {
-    center: {
-        latitude: 45,
-        longitude: -73
-    },
-    zoom: 8
-};
+    $scope.cities = [];
+    $scope.map;
+    $scope.infoBox = new google.maps.InfoWindow();
+    var mapContainer = document.getElementById('map');
+    mapContainer.style.width = '70%';
+    mapContainer.style.height = '500px';
+
+    $http.get('../db/data.json').success(function(data) {
+        $scope.cities = data;
+    });
+
+    $scope.initialize = function() {
+        var mapOptions = {
+            center: new google.maps.LatLng(50.5, 30.5),
+            zoom: 8,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        $scope.map = new google.maps.Map(mapContainer, mapOptions);
+    }
+
+    $scope.showCity = function(city) {
+        var coords = new google.maps.LatLng(city.lat, city.lng);
+        $scope.infoBox.setContent(city.city + ' - ' + city.desc);
+        $scope.infoBox.setPosition(coords);
+        $scope.infoBox.open($scope.map);
+        $scope.map.setCenter(coords);
+    }
   
 $http.post('../server/main.php', {loginHandlerAction: "getuser"}).success(function(data){
  //       console.log('get user response', data);
@@ -19,4 +39,10 @@ $http.post('../server/main.php', {loginHandlerAction: "getuser"}).success(functi
         $scope.username = data1;
         console.log("Map",$scope.username);
       });
+
+
   });
+
+
+
+
