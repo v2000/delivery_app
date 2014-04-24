d54c11e34c4fa2453e7e8a254d0adbbfb49e3121
MrsColombo <julia34567@hotmail.com>
2014-04-24 20:34:04 +0200

    Our project is now based on angular-seed

---
+++ app/js/controllers/controllers.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('woodelivery.controllers', [])
+  .controller('MyCtrl1', [function() {
+
+  }])
+  .controller('MyCtrl2', [function() {
+
+  }]);
